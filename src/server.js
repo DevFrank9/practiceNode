36284d5b10271ee87208d9fd47271b3a146c7007
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-10-29 23:09:24 +0900

    서버연결2

--- src/server.js
+++ src/server.js
@@ -2,6 +2,4 @@ import express from "express";
 
 const app = express();
 
-const handleListening = () => console.log("Server listening on port 4000");
-
-app.listen(4000, handleListening);
+app.listen(4000, () => console.log("Server listening on port 4000"));
