887a94dd50ccde82d1ec916996bfa72d7aa9f22c
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-01 01:05:48 +0900

    morgan 설치

--- src/server.js
+++ src/server.js
@@ -3,14 +3,6 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const routerLogger = (req, res, next) => {
-  console.log(`PATH ${req.path}`);
-  next();
-};
-const methodLogger = (req, res, next) => {
-  console.log(`METHOD ${req.method}`);
-  next();
-};
 const handleHome = (req, res) => {
   console.log("I will respond");
   return res.send("hello!");
@@ -18,7 +10,6 @@ const handleHome = (req, res) => {
 const login = (req, res) => {
   return res.send("Log in");
 };
-app.use(methodLogger, routerLogger);
 app.get("/", handleHome);
 app.get("/login", login);
 
