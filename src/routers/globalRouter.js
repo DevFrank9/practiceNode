b80bf4c9fe7821888782636b313f2b9b0e514cb7
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-01 04:08:02 +0900

    라우터 세분화 적용

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { trending } from "../controllers/videoController";
-import { join } from "../controllers/userController";
+import { trending, search } from "../controllers/videoController";
+import { join, login } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
@@ -9,5 +9,7 @@ const handleJoin = (req, res) => res.send("Join");
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
