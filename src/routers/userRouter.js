52728acab5a30e5992307588a8afbb77f17e5cb9
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-01 03:13:23 +0900

    컨트롤러 , 라우터 분리 및 import/export 사용으로 아키텍쳐 설계

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,9 @@
 import express from "express";
+import { remove, edit } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-const handleEditUser = (req, res) => {
-  return res.send("Edit user");
-};
-
-userRouter.get("/edit", handleEditUser);
+userRouter.get("/edit", edit);
+userRouter.get("/remove", remove);
 
 export default userRouter;
