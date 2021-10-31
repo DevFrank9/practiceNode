8ad66cfd2ae96bdfd62277caf244ea4565e947f1
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-01 01:50:55 +0900

    라우터를 받아 생성하는 방법

--- src/server.js
+++ src/server.js
@@ -8,14 +8,28 @@ const logger = morgan("dev");
 app.use(logger);
 
 const globalRouter = express.Router();
+const handleHome = (req, res) => {
+  return res.send("Home");
+};
+globalRouter.get("/", handleHome);
+
 const userRouter = express.Router();
+const handleEditUser = (req, res) => {
+  return res.send("Edit user");
+};
+userRouter.get("/edit", handleEditUser);
+
 const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => {
+  return res.send("Watch Video");
+};
+videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
