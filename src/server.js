4c4397daec427fe70bd91924e020ed317ff50eb2
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-10-31 22:19:51 +0900

    protected url 이동시 middleware 사용

--- src/server.js
+++ src/server.js
@@ -4,16 +4,30 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Some one is going to: ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 };
-
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue");
+  next();
+};
 const handleHome = (req, res) => {
   return res.send("I love middleware");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+const handleProtected = (req, res) => {
+  return res.send("Wlecome to the private lounge");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
