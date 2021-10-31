846706a6abdbd0c417b0d0812267a86bf472e0f9
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-01 01:39:22 +0900

    라우터 생성

--- src/server.js
+++ src/server.js
@@ -2,20 +2,18 @@ import express from "express";
 import morgan from "morgan";
 
 const PORT = 4000;
+
 const app = express();
 const logger = morgan("dev");
-
-const handleHome = (req, res) => {
-  console.log("I will respond");
-  return res.send("hello!");
-};
-const login = (req, res) => {
-  return res.send("Log in");
-};
-
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", login);
+
+const globalRouter = express.Router();
+const userRouter = express.Router();
+const videoRouter = express.Router();
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
