e37f3f9747678b367c8994ff9941f9d33ec759ae
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-03 23:13:13 +0900

    MongoDB에 프론트엔드에서 보낸 폼 저장하기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,6 +4,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
+  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
@@ -24,9 +25,9 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
+  await Video.create({
     title,
     description,
     createdAt: Date.now(),
