fd12a33ecc02b2cf53e2de92ac5d52d8b91588ad
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-04 09:24:59 +0900

    delete 기능 구현

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -59,3 +59,10 @@ export const postUpload = async (req, res) => {
     });
   }
 };
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  //delete Video
+  return res.redirect("/");
+};
