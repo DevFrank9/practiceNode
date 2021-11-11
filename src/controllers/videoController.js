c751ae1accd0c3b9af0ce1d9674e5b8261e5ed69
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-11 15:20:26 +0900

    비디오 업로드 기능 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -43,11 +43,13 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = async (req, res) => {
+  const { path: fileUrl } = req.file;
+  const { title, description, hashtags } = req.body;
   try {
-    const { title, description, hashtags } = req.body;
     await Video.create({
       title,
       description,
+      fileUrl,
       hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
