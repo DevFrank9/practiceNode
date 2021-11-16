6ca1fa06bc00bcf3074b425c068e729f30f0e4cc
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2021-11-16 19:45:28 +0900

    ffmpeg를 이용하여 webm파일을 mp4파일로 인코딩하여 저장

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,4 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,10 +6,22 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({
+    corePath: "https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js",
+    log: true,
+  });
+  await ffmpeg.load();
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+
+  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const mp4Url = URL.createObjectURL(mp4Blob);
+
   const a = document.createElement("a");
-  a.href = videoFile;
-  a.download = "MyRecording.webm";
+  a.href = mp4Url;
+  a.download = "MyRecording.mp4";
   document.body.appendChild(a);
   a.click();
 };
@@ -19,13 +32,13 @@ const handleStop = () => {
   startBtn.addEventListener("click", handleDownload);
   recorder.stop();
 };
+
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
   recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
   recorder.ondataavailable = (event) => {
-    videoFile = URL.createObjectURL(event.data);
     videoFile = URL.createObjectURL(event.data);
     video.srcObject = null;
     video.src = videoFile;
@@ -34,6 +47,7 @@ const handleStart = () => {
   };
   recorder.start();
 };
+
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
@@ -42,5 +56,7 @@ const init = async () => {
   video.srcObject = stream;
   video.play();
 };
+
 init();
+
 startBtn.addEventListener("click", handleStart);
