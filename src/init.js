6ca6818245fc4dbde9b126e261ef278c2d19174e
Jongho Lee <90244663+DevFrank9@users.noreply.github.com>
2022-01-10 15:04:41 +0900

    주석작업 시작

--- src/init.js
+++ src/init.js
@@ -6,10 +6,13 @@ import "./models/Video";
 import "./models/User";
 import "./models/Comment";
 
+// app이란 변수로 server.js 담기
 import app from "./server";
 
+// 포트번호 할당
 const PORT = process.env.PORT || 4000;
 
+// 포트 연결 성공시 log 띄우기
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
--- src/server.js
+++ src/server.js
@@ -14,15 +14,19 @@ import apiRouter from "./routers/apiRouter";
 /* middle ware */
 import { embedder, localsMiddleware } from "./middlewares";
 
+// 서버 구현 위한 express 프레임워크 불러오기
 const app = express();
+// 위의 구동 현황을 보여주기 위해 로거 사용
 const logger = morgan("dev");
 
+// 웹 뷰 렌더링 프레임워크로 pug사용한다고 선언
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// express 로 구동되는 db등의 일회성 세션 정보 사용한다고 선언
 app.use(
   session({
     secret: process.env.COOKIE_SECRET,
@@ -32,6 +36,7 @@ app.use(
   })
 );
 
+// root부터 각 1차적인 라우트들 알려주기
 app.use(flash());
 app.use(embedder);
 app.use(localsMiddleware);
